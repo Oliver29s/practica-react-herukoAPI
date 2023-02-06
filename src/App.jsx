77f9733307650b1345813a804d69d757a0316b2b
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-02-06 13:20:25 -0500

    crear manejo de errorecon catch

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,41 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import CaatchError from "./components/CaatchError";
 import CardRick from "./components/CardRick";
+import Form from "./components/Form";
 import LocationRick from "./components/LocationRick";
 import useNumberRandom from "./utili/useNumberRandom";
 
 function App() {
   const [allElemts, setallElemt] = useState("");
   const [event, setEvent] = useState(useNumberRandom());
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(true);
   useEffect(() => {
     axios
       .get(`https://rickandmortyapi.com/api/location/${event}`)
-      .then((res) => setallElemt(res.data), setError(true))
-      .catch((err) => console.log(err), setError(false));
+      .then((res) => {setallElemt(res.data),setError(true)})
+      .catch((err) => {console.log(err),
+        setError(false) });
   }, [event]);
 
   return (
     <div className="App">
       <header className="contain__header">
-      <h1>Rick and Morty</h1>
+        <h1>Rick and Morty</h1>
       </header>
-    
-      <LocationRick allElemts={allElemts} setEvent={setEvent} />
-      {
-      error ? 
-      <CaatchError />
-      : 
-      allElemts?.residents?.map((url) => <CardRick key={url} url={url} />)
-      
-      }
+      <Form setEvent={setEvent} />
+
+      {error ? (
+        <>
+          <LocationRick allElemts={allElemts} />
+          <div className="contain__cards">
+            {allElemts?.residents?.map((url) => (
+              <CardRick key={url} url={url} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <CaatchError />
+      )}
     </div>
   );
 }
--- src/components/LocationRick.jsx
+++ src/components/LocationRick.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Form from "./Form";
 
-const LocationRick = ({ allElemts, setEvent }) => {
+const LocationRick = ({ allElemts}) => {
   return (
     <>
      
@@ -9,15 +9,15 @@ const LocationRick = ({ allElemts, setEvent }) => {
         <h2>Name: {allElemts?.name}</h2>
         <div className="section__info">
         <div className="bxs-rule">
-          <i class='bx bxs-universal-access'></i>
+          <i className='bx bxs-universal-access'></i>
             <p>Type: {allElemts?.type}</p>
           </div>
           <div className="bxs-rule"> 
-          <i class='bx bxs-ruler'></i>
+          <i className='bx bxs-ruler'></i>
             <p>Dimenssion: {allElemts?.dimension}</p>
           </div>
           <div className="bxs-rule">
-          <i class='bx bxs-home-alt-2'></i>
+          <i className='bx bxs-home-alt-2'></i>
             <p>Residents: {allElemts?.residents?.length}</p>
           </div>
         </div>
@@ -25,7 +25,7 @@ const LocationRick = ({ allElemts, setEvent }) => {
        
         
       </section>
-      <Form setEvent={setEvent} />
+      
     </>
   );
 };
